9b694dd97a20413f10ea65e34fe22f5e01432229
Andrew Hibbard <andrew.o.hibbard@gmail.com>
2020-01-01 21:29:08 -0500

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -35,22 +35,36 @@ class ApplicationController < Sinatra::Base
     erb :'songs/index'
   end 
 
+  get '/songs/new' do 
+    erb :'/songs/new'
+  end 
+
   get '/songs/:slug' do
     @song = Song.find_by_slug(params[:slug])
     # @artist = @song.artist.name
     erb :'songs/show'
   end 
 
-  get '/songs/new' do 
-    erb :'/songs/new'
-  end 
-
-  post "/songs" do
-    @song = Song.new(params[:song][:name])
-    @song.artist = Artist.find_or_create_by(name: params[:artist][:name])
+  post '/songs' do
+    @song = Song.create(params[:song])
+    @song.artist = Artist.find_or_create_by(:name => params[:artist][:name])
     @song.genre_ids = params[:genres]
     @song.save
-    redirect '/songs/#{@song.slug}'
+    redirect("/songs/#{@song.slug}")
+  end 
+
+  get '/songs/:slug/edit' do
+    @song = Song.find_by_slug(params[:slug])
+    erb :'songs/edit'
+  end
+
+  patch '/songs/:slug' do
+    @song = Song.find_by_slug(params[:slug])
+    @song.update(params[:song])
+    @song.artist = Artist.find_or_create_by(:name => params[:artist][:name])
+    @song.genre_ids = params[:genres]
+    @song.save 
+    redirect("/songs/#{@song.slug}")
   end 
 
 end 

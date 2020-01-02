b5ee1d65650286e1ab4c587c98808223c58ba916
Andrew Hibbard <andrew.o.hibbard@gmail.com>
2020-01-01 19:37:13 -0500

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < Sinatra::Base
   register Sinatra::ActiveRecordExtension
+  enable :sessions
   set :session_secret, "my_application_secret"
   set :views, Proc.new { File.join(root, "../views/") }
 
@@ -7,10 +8,7 @@ class ApplicationController < Sinatra::Base
     erb :index
   end
 
-  get '/songs' do 
-    @songs = Song.all
-    erb :'songs/index'
-  end 
+
 
   get '/artists' do
     @artists = Artist.all
@@ -27,27 +25,32 @@ class ApplicationController < Sinatra::Base
     erb :'artists/show'
   end
 
-  get '/songs/:slug' do
-    @song = Song.find_by_slug(params[:slug])
-    @artist = @song.artist.name
-    erb :'songs/show'
-  end 
-
   get '/genres/:slug' do
     @genre = Genre.find_by_slug(params[:slug])
     erb :'genres/show'
   end 
 
-  get '/songs/new' do 
-    erb :'songs/new'
+  get '/songs' do 
+    @songs = Song.all
+    erb :'songs/index'
   end 
 
-  post '/songs' do
-    @song = Song.create(params[:song])
+  get '/songs/:slug' do
+    @song = Song.find_by_slug(params[:slug])
+    # @artist = @song.artist.name
+    erb :'songs/show'
+  end 
+
+  get '/songs/new' do 
+    erb :'/songs/new'
+  end 
+
+  post "/songs" do
+    @song = Song.new(params[:song][:name])
     @song.artist = Artist.find_or_create_by(name: params[:artist][:name])
     @song.genre_ids = params[:genres]
     @song.save
-    rediect '/songs/#{@song.slug}'
+    redirect '/songs/#{@song.slug}'
   end 
 
 end 

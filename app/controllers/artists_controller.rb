89f6df26b79f21aeeff55489d3478fc1a9c71807
jessesbyers <jesse.s.byers@gmail.com>
2020-01-06 14:23:34 -0500

    Done.

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -4,4 +4,12 @@ class ArtistsController < ApplicationController
         @artists = Artist.all 
         erb :'artists/index'
     end
+
+    get '/artists/new' do
+    end
+
+    get '/artists/:slug' do
+        @artist = Artist.find_by_slug(params[:slug])
+        erb :'/artists/show'
+    end
 end
--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -4,4 +4,26 @@ class SongsController < ApplicationController
         @songs = Song.all
         erb :'/songs/index'
     end
+
+    get '/songs/new' do
+        @genres = Genre.all
+        erb :'/songs/new'
+    end
+
+
+    # working on this method - spec 5
+    post '/songs' do
+        binding.pry
+        artist = Artist.create(:name => params[:artist])
+        song = Song.new(:name => params[:name]) 
+        song.artist = artist
+        # song.genres = params[:genre_ids].to_i
+        song.save
+        erb :'songs/#{song.slug}'
+    end
+
+    get '/songs/:slug' do
+        @song = Song.find_by_slug(params[:slug])
+        erb :'/songs/show'
+    end
 end

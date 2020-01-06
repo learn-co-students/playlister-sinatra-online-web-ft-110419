308661c585d5bba58ad9c74fe8656afffec1773d
jessesbyers <jesse.s.byers@gmail.com>
2020-01-06 11:32:25 -0500

    Done.

--- app/models/songgenre.rb
+++
@@ -1,4 +0,0 @@
-class SongGenre < ActiveRecord::Base
-    has_many :songs
-    has_many :genres
-end
--- db/migrate/20200106161251_create_song_genres.rb
+++ db/migrate/20200106161251_create_song_genres.rb
@@ -1,4 +1,4 @@
-class CreateSongsGenres < ActiveRecord::Migration[5.2]
+class CreateSongGenres < ActiveRecord::Migration[5.2]
   def change
     create_table :song_genres do |t|
       t.integer :song_id
@@ -6,3 +6,4 @@ class CreateSongsGenres < ActiveRecord::Migration[5.2]
     end
   end
 end
+
